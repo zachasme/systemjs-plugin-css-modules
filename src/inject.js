948f102cf7d6c4480f31ca172af796b3e271b643
Zacharias Knudsen <zachasme@gmail.com>
2016-02-01 10:08:50 +0100

    Guard inject in non-browsers

--- src/inject.js
+++ src/inject.js
@@ -2,6 +2,7 @@
  * Inject css source string into document
  */
 export default function inject(css) {
+  if (typeof window != 'undefined') return
   const element = document.createElement('style')
   element.innerHTML = css
   document.head.appendChild(element)
